c9f684d4f5131198a1ee6bfb32cf63212c93f446
camilaaylenricardo <163005132+camilaaylenricardo@users.noreply.github.com>
2024-06-24 20:51:40 -0300

    Delete js11 directory

--- js11/ejercicio03/main.mjs
+++
@@ -1,9 +0,0 @@
-import Calculadora from './calculadora.mjs';
-
-const calculadora = new Calculadora();
-
-const resultadoMultiplicacion = calculadora.multiplicar(70, 10);
-const resultadoDivision = calculadora.dividir(28, 7);
-
-console.log(`Resultado de multiplicar 70 y 10: ${resultadoMultiplicacion}`);
-console.log(`Resultado de dividir 28 entre 7: ${resultadoDivision}`);
