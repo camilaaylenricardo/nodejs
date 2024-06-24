c9f2750b61ddd064993540bf61eb03f0abb38a31
camilaaylenricardo <163005132+camilaaylenricardo@users.noreply.github.com>
2024-06-24 20:36:22 -0300

    Add files via upload

---
+++ js11/ejercicio02/persona.js
@@ -0,0 +1,12 @@
+class Persona {
+    constructor(nombre, edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    mostrarInformacion() {
+        console.log(`Nombre: ${this.nombre}, Edad: ${this.edad}`);
+    }
+}
+
+module.exports = Persona;
