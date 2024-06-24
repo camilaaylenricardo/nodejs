f2c9dd36a961d91229ceef497ec55f257c644772
camilaaylenricardo <163005132+camilaaylenricardo@users.noreply.github.com>
2024-06-24 20:48:42 -0300

    Add files via upload

---
+++ js11/ejercicio02/app.js
@@ -0,0 +1,9 @@
+
+const Persona = require('./persona');
+
+const persona1 = new Persona('Camila', 18);
+const persona2 = new Persona('Juan', 19);
+
+
+persona1.mostrarInformacion(); 
+persona2.mostrarInformacion(); 
---
+++ js11/ejercicio03/calculadora.mjs
@@ -0,0 +1,11 @@
+class Calculadora {
+    multiplicar(a, b) {
+        return a * b;
+    }
+
+    dividir(a, b) {
+        return a / b;
+    }
+}
+
+export default Calculadora;
